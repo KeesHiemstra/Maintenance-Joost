77f3c158772e608c4204832fbfcbff12835ec3ea
Kees Hiemstra <KeesHiemstra@users.noreply.github.com>
2021-01-29 15:01:54 +0100

    Version 2.0.0.15 (2021-01-29) * Use the renamed article ('Oploskoffie, pot'). * Moved to Maintenance Joost solution and renamed MaintJournal to MaintenanceJournal.

---
+++ MaintenanceJournal/Views/CoffeeUsageWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MaintenanceJournal.ViewModels;
+
+using System.Windows;
+
+namespace MaintenanceJournal.Views
+{
+	/// <summary>
+	/// Interaction logic for CoffeeUsageWindow.xaml
+	/// </summary>
+	public partial class CoffeeUsageWindow : Window
+	{
+		public CoffeeUsageWindow(CoffeeUsageViewModel coffeeUsageViewModel)
+		{
+			InitializeComponent();
+		}
+	}
+}
