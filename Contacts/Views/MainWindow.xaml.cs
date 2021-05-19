d716ab73cb950ef0833513de1eeecd05dc116038
Kees Hiemstra <KeesHiemstra@users.noreply.github.com>
2021-05-19 08:43:34 +0200

    WIP Contacts

---
+++ Contacts/Views/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Contacts
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		#region [ Menu methods ]
+
+		#region Exit command
+		private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void ExitCommand_Execute(object sender, ExecutedRoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+		#endregion
+
+		#region NewContact command
+
+		private void NewContactCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void NewContactCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			//VM.EditContact(null);
+		}
+
+		#endregion
+
+		#endregion
+	}
+}
