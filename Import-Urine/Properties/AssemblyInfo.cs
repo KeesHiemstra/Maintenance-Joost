84f5af8a88d3ae86b163745661c021610f312867
Kees Hiemstra <KeesHiemstra@users.noreply.github.com>
2026-03-18 13:10:43 +0100

    Update to .Net 10

--- Import-Urine/Properties/AssemblyInfo.cs
+++ Import-Urine/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CHi")]
 [assembly: AssemblyProduct("Maintenance Joost")]
-[assembly: AssemblyCopyright("Copyright © Kees Hiemstra 2022")]
+[assembly: AssemblyCopyright("Copyright © Kees Hiemstra 2022-2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.3")]
-[assembly: AssemblyFileVersion("1.0.1.3")]
+[assembly: AssemblyVersion("1.0.1.4")]
+[assembly: AssemblyFileVersion("1.0.1.4")]
