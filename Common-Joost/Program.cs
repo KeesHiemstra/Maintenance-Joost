a9a3c0278d96a980850a1ed2d9fbd42bc09860a0
Kees Hiemstra <KeesHiemstra@users.noreply.github.com>
2026-04-05 10:49:46 +0200

    Everything upgraded to net10.0 or framework 4.8.1

--- Common-Joost/Program.cs
+++ Common-Joost/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Export_UsedArticles
+﻿namespace Export_UsedArticles
 {
 	class Program
 	{
